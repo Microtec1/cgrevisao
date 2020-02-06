7c75138acdd42625853dec3beb8a9e47e2ca6c86
Microtec1 <shirv@bol.com.br>
2020-02-06 14:47:31 -0300

    arquivo para criptografia em banco

---
+++ banco.sql
@@ -0,0 +1,17 @@
+-- entrar no shell
+mysql -u 
+
+--visualizar os bancos existentes
+show databases;
+
+-- criar banco de dados
+create database aula2git;
+
+-- selecionar uma base de dados
+use aula2git;
+
+-- criar uma tabela no banco de dados
+create table usuario(
+    idusuario int primary key auto_increment,
+
+);
---
+++ gravar.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    // pegar os dados da tela
+    $email = $_POST["email"];
+    $senha = md5($_POST["senha"]);
+
+    /*
+    echo "E-mail :".$email,"<br>";
+    echo "Senha :".$senha;
+    
+    */
+
+    // gravar num banco
+
+
+    ?>
+    <!-- fora do php -->
+</body>
+</html>
